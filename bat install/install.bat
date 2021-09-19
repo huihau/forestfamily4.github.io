c95f6a10e555fe8a0dd2d1d0ca6a7fbf30846d1c
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2021-09-19 11:23:09 +0900

    update

--- bat install/install.bat
+++ bat install/install.bat
@@ -1,10 +1,12 @@
-bitsadmin /transfer download https://github.com/battlecatsultimate/bcu-assets/blob/master/jar/BCU-Initializer.jar?raw=true %CD%\BCU-Initializer.jar
-bitsadmin /transfer download https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.zip %CD%\java.zip
-bitsadmin /transfer download https://ja.osdn.net/frs/redir.php?m=nchc&f=sevenzip%2F70662%2F7z1900.exe %CD%\7za.exe
-7za.exe x java.zip
-set Path=%Path%;%CD%\jdk-17\bin
-del java.zip
-del 7za.exe
-java -jar BCU-Initializer.jar
-bitsadmin /transfer download https://raw.githubusercontent.com/forestfamily4/forestfamily4.github.io/main/bat%20install/start.bat %CD%\start.bat
+md BCU
+bitsadmin /transfer download https://github.com/battlecatsultimate/bcu-assets/blob/master/jar/BCU-Initializer.jar?raw=true %CD%\BCU\BCU-Initializer.jar
+bitsadmin /transfer download https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.zip %CD%\BCU\java.zip
+bitsadmin /transfer download https://ja.osdn.net/frs/redir.php?m=nchc&f=sevenzip%2F70662%2F7z1900.exe %CD%\BCU\7za.exe
+%CD%\BCU\7za.exe x %CD%\BCU\java.zip
+set Path=%Path%;%CD%\BCU\jdk-17\bin
+del %CD%\BCU\java.zip
+del %CD%\BCU\7za.exe
+java -jar %CD%\BCU\BCU-Initializer.jar
+bitsadmin /transfer download https://raw.githubusercontent.com/forestfamily4/forestfamily4.github.io/main/bat%20install/start.bat %CD%\BCU\start.bat
+@PAUSE
 del "%~f0"
